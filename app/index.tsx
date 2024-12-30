e0612e0c35b789936253466398a0613ae3b3a5be
jblSomoza <jhony-bryan@outlook.com>
2024-12-29 23:33:15 -0600

    Agregar colores personalizados y actualizar estilos de texto en la aplicación

--- app/index.tsx
+++ app/index.tsx
@@ -6,13 +6,13 @@ const App = () => {
   return (
     <SafeAreaView >
       <View>
-        <Text className='text-3xl' style={{ fontFamily: 'WorkSans-Black'}}>HelloWave</Text>
+        <Text className='text-3xl text-primary' style={{ fontFamily: 'WorkSans-Black'}}>HelloWave</Text>
 
-        <Text className='text-3xl font-work-black' >HelloWave</Text>
+        <Text className='text-3xl text-secondary font-work-black' >HelloWave</Text>
 
-        <Text className='text-3xl font-work-medium'>HelloWave</Text>
+        <Text className='text-3xl text-tertiary font-work-medium'>HelloWave</Text>
 
-        <Text className='text-3xl font-work-light'>HelloWave</Text>
+        <Text className='text-3xl text-secondary-200 font-work-light'>HelloWave</Text>
       </View>
     </SafeAreaView>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+
+      colors: {
+        primary: '#49129C',
+        secondary: {
+          DEFAULT: '#B40086',
+          100: '#C51297',
+          200: '#831266'
+        },
+        tertiary: {
+          DEFAULT: '#EF2967',
+        }
+      },
+
       fontFamily: {
         'work-black': ['WorkSans-Black', 'sans-serif'],
         'work-light': ['WorkSans-Light', 'sans-serif'],
