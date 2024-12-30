dd63fa1fb1fbbec35ec6dded4e3256f0726f1674
jblSomoza <jhony-bryan@outlook.com>
2024-12-29 23:28:10 -0600

    Agregar soporte para fuentes personalizadas y mejorar la estructura del layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,30 @@
-import { Slot } from "expo-router";
+import { Slot, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
 
-import "../global.css";
+import "./global.css";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+
+  const [fontsLoaded, error ] = useFonts({
+    'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
+    'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
+    'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf'),
+  });
+
+  useEffect(() => {
+
+    if(error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
   return (
     <Slot />
   )
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const App = () => {
   return (
-    <View className='mt-10'>
-      <Text>HelloWave</Text>
-    </View>
+    <SafeAreaView >
+      <View>
+        <Text className='text-3xl' style={{ fontFamily: 'WorkSans-Black'}}>HelloWave</Text>
+
+        <Text className='text-3xl font-work-black' >HelloWave</Text>
+
+        <Text className='text-3xl font-work-medium'>HelloWave</Text>
+
+        <Text className='text-3xl font-work-light'>HelloWave</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
