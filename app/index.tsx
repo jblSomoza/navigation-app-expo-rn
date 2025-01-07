ef8c8cd2d80c1a86dad94732837c685c80d4367f
jblSomoza <jhony-bryan@outlook.com>
2025-01-06 21:41:10 -0600

    Agregar navegación por pestañas y pantallas de inicio y favoritos

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { Redirect } from 'expo-router'
 
 const App = () => {
-  return <Redirect href="/(stack)/home" />
+  // return <Redirect href="/(stack)/home" />
+  return <Redirect href="/tabs" />
 }
 
 export default App
---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,28 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'purple' }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          // tabBarShowLabel: false,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home-outline" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: 'Favorites',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="star-outline" color={color} />,
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default TabsLayout
