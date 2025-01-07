7b954f478422041b1c154bfd031266383fb843f4
jblSomoza <jhony-bryan@outlook.com>
2025-01-06 22:15:28 -0600

    Agregar opciones de estilo y iconos a la navegación del cajón en la aplicación

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -2,16 +2,29 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 const DrawerLayout = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer
+        screenOptions={{
+          overlayColor: 'rgba(0,0,0,0.4)', // This is the color of the overlay when the drawer is open
+          drawerActiveTintColor: 'indigo', // This is the color of the text when the drawer is open
+          sceneStyle: {
+            backgroundColor: 'white', // This is the background color of the drawer
+          },
+          headerShadowVisible: false
+        }}
+      >
         <Drawer.Screen
           name="user/index" // This is the name of the page and must match the url from root
           options={{
             drawerLabel: 'User',
             title: 'overview',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color}  size={size} />
+            ),
           }}
         />
         <Drawer.Screen
@@ -19,6 +32,9 @@ const DrawerLayout = () => {
           options={{
             drawerLabel: 'Schedule',
             title: 'Horarios',
+            drawerIcon: () => (
+              <Ionicons name="calendar" size={24} color="black" />
+            ),
           }}
         />
       </Drawer>
